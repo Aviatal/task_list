081bed60dc2d11b0bfeda27f8618b21856e8cae7
Paweł Budek <pbudek1908@gmail.com>
2025-02-23 19:59:38 +0100

    Refactor store and update methods to use validated data directly

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -24,18 +24,14 @@ class TasksController extends Controller
 
     public function store(TaskRequest $request): \Illuminate\Http\RedirectResponse
     {
-        $request->validated();
-
-        $newTask = Task::query()->create($request->except('_token'));
+        $newTask = Task::query()->create($request->validated()->except('_token'));
         return redirect()->route('tasks.show', ['task' => $newTask->id])
             ->with('success', 'Task created successfully');
     }
 
     public function update(TaskRequest $request, Task $task): \Illuminate\Http\RedirectResponse
     {
-        $request->validated();
-
-        $task->update($request->except('_token'));
+        $task->update($request->validated()->except('_token'));
         return redirect()->route('tasks.show', ['task' => $task->id])
             ->with('success', 'Task updated successfully');
     }
